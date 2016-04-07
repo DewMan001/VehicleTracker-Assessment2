a08fb2bd1e46912dc10cb2c5b4a23cee82aafb32
James Burton <deweymanster@gmail.com>
2016-04-07 10:51:03 +0800

    +Set default values for vehicle variables
    >Next: Playing around with a copy constructor.

--- RentNRideVehicleTracker-Assessment2/src/rentnridevehicletracker/assessment2/Vehicle.java
+++ RentNRideVehicleTracker-Assessment2/src/rentnridevehicletracker/assessment2/Vehicle.java
@@ -10,5 +10,28 @@ package rentnridevehicletracker.assessment2;
  * @author dewey
  */
 public class Vehicle {
+    static String manufacturer;
+    static String model;
+    static int make_Year;
+    static double kilometers;
+    static int services;
     
+    //setting up the default values
+    Vehicle()
+    {
+    manufacturer = "No manufacturer name given";
+    model = "No model set";
+    make_Year = 0;
+    kilometers = 0.00;
+    services = 0;
+    }
+    //a copy constructor
+    Vehicle(Vehicle First)
+    {
+        manufacturer = Vehicle.manufacturer;
+        model = Vehicle.model;
+        make_Year = Vehicle.make_Year;
+        kilometers = Vehicle.kilometers;
+        services = Vehicle.services;
+    }
 }
