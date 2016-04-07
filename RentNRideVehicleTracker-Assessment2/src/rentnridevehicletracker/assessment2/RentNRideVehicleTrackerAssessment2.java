27fcc306dae94c1bbe9be57fcb9e58eddc2cd0b3
James Burton <deweymanster@gmail.com>
2016-04-07 10:04:13 +0800

    InitialCommit

---
+++ RentNRideVehicleTracker-Assessment2/src/rentnridevehicletracker/assessment2/RentNRideVehicleTrackerAssessment2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rentnridevehicletracker.assessment2;
+
+/**
+ *
+ * @author dewey
+ */
+public class RentNRideVehicleTrackerAssessment2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //This is for git stuff
+        
+        String git;
+        git = "fuck you github";
+    }
+    
+}
