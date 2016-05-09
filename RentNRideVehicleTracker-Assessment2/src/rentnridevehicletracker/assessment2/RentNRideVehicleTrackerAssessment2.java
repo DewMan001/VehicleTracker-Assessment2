8c0f9310d7fd2c7c701945816f5fcc232d10d600
DewMan001 <deweymanster@gmail.com>
2016-05-09 10:35:16 +0800

    +Added the ability to staple a journey to a given vehicle.
    +Added toString methods for each class to make printing those stapled-on
    journies and vehicles that little bit easier. (now with prettier line grammar).
    
    >Coming up:When printing out the journies attached to the vehicles, it's
    printing out the same journey for all ten elements in the array. Need to
    get it to print it the once, and see if there is in fact anything in the next
    element.

--- RentNRideVehicleTracker-Assessment2/src/rentnridevehicletracker/assessment2/RentNRideVehicleTrackerAssessment2.java
+++ RentNRideVehicleTracker-Assessment2/src/rentnridevehicletracker/assessment2/RentNRideVehicleTrackerAssessment2.java
@@ -18,17 +18,40 @@ public class RentNRideVehicleTrackerAssessment2 {
         // TODO code application logic here
         //This is for git stuff
         
+        
+   
+        Journey[] journey1 = new Journey[10];
+        
+        Journey journeyx = new Journey();
+        
+        journeyx.setCar("Holden HQ");
+        journeyx.setStartLocation("Tuscon, Arizona");
+        journeyx.setEndLocation("Las Vegas, Nevada");
+        journeyx.setTotalDistance(360);
+        journeyx.setUsedFuel(48);
+        
+        journey1[0] = journeyx;
+        
+        journey1[0].printJourney();
+        
+      
+        
+        System.out.println(" ");
+        System.out.println(" ");
+        
         Vehicle Car1 = new Vehicle();
         
         //setting up all of the variables
         Car1.setManufacturer("Holden");
-        Car1.setModel("DEFEAT");
+        Car1.setModel("HQ");
         Car1.setServices(2);
-        Car1.setMakeYear(2015);
+        Car1.setMakeYear(1987);
         Car1.setKM(0.05);
+        Car1.AddJourney(journey1[0]);
         
         //printing out the vehicle details
         Car1.printVehicle();
+        
     }
     
 }
